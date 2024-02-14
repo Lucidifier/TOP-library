525ba5a53d522706278dc28b6e59f5916a6ab267
Lucidifier <dragan.hardi3@gmail.com>
2024-02-14 01:32:35 +0100

    add  skeleton  addBook  functionality

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
+  const library = [];
+
   const openDialogBtn = document.querySelector('.open-dialog');
   const closeDialogBtn = document.querySelector('.close-dialog');
+  const addBookBtn = document.querySelector('.add-book');
   const dialog = document.querySelector('.entry-dialog');
+  const bookPlaceholder = document.querySelector('book-placeholder');
+
+
+
+  function Book(title, author, genre, read) {
+    this.name = title;
+    this.author  = author;
+    this.genre = genre;
+    this.read = function() {
+      if(read===true) {
+        this.read = true;
+      };
+    };
+  }
+
+  function addBookToLibrary() {
+    let newBook = new Book('aa', 'aaa', 'aaaa', 1);
+    library.push(newBook);
+    console.log(library);
+  }
 
   openDialogBtn.addEventListener('click', () => {
     console.log('asd');
@@ -12,4 +34,5 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('aaa');
     dialog.close();
   });
-})
+
+  addBookBtn.addEventListener('click', addBookToLibrary);
