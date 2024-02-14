cbf3a7489dbe7ddb7ed04201f8e041b5c004713b
Lucidifier <dragan.hardi3@gmail.com>
2024-02-14 01:06:39 +0100

    add  dialog

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-console.log('asd');
+document.addEventListener('DOMContentLoaded', function() {
+  const openDialogBtn = document.querySelector('.open-dialog');
+  const closeDialogBtn = document.querySelector('.close-dialog');
+  const dialog = document.querySelector('.entry-dialog');
+
+  openDialogBtn.addEventListener('click', () => {
+    console.log('asd');
+    dialog.showModal();
+  });
+
+  closeDialogBtn.addEventListener('click', () => {
+    console.log('aaa');
+    dialog.close();
+  });
+})
