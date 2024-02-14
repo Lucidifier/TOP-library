c4b56944a1666ee94687ef2887033f6436bd1405
Lucidifier <dragan.hardi3@gmail.com>
2024-02-14 02:44:03 +0100

    finish addBook functionality, make  basic displayBook functionality

--- scripts.js
+++ scripts.js
@@ -1,28 +1,61 @@
   const library = [];
 
+  function displayBook() {
+    library.forEach(function(item) {
+      let containerDiv = document.createElement('div');
+      let line1 = document.createElement('p');
+      let line2 = document.createElement('p');
+      let line3 = document.createElement('p');
+      line1.innerText = item.title;
+      line2.innerText = item.author;
+      line3.innerText = item.genre;
+      bookContainer.appendChild(containerDiv);
+      containerDiv.appendChild(line1);
+      containerDiv.appendChild(line2);
+      containerDiv.appendChild(line3);
+      containerDiv.classList.toggle('book');
+    })
+  }
+
+  console.log(library);
+ 
   const openDialogBtn = document.querySelector('.open-dialog');
   const closeDialogBtn = document.querySelector('.close-dialog');
   const addBookBtn = document.querySelector('.add-book');
   const dialog = document.querySelector('.entry-dialog');
-  const bookPlaceholder = document.querySelector('book-placeholder');
+  const bookContainer = document.querySelector('.book-container');
+  const testButton = document.querySelector('.test-btn');
+
+  //Input fields 
+  const bookTitle = document.getElementById('title');
+  const bookAuthor = document.getElementById('author');
+  const bookGenre = document.getElementById('genre');
+  const bookRead = document.getElementById('read');
 
 
-
-  function Book(title, author, genre, read) {
-    this.name = title;
-    this.author  = author;
-    this.genre = genre;
-    this.read = function() {
-      if(read===true) {
-        this.read = true;
-      };
-    };
+  function Book() {
+    this.title = bookTitle.value;
+    this.author  = bookAuthor.value;
+    this.genre = bookGenre.value;
+    this.read = bookRead.checked;
   }
 
   function addBookToLibrary() {
-    let newBook = new Book('aa', 'aaa', 'aaaa', 1);
+
+    //Reset values because dialog box remembers  old ones
+    function resetValues() {
+      bookTitle.value = '';
+      bookAuthor.value = '';
+      bookGenre.value = '';
+      bookRead.checked = false;
+    }
+
+    let newBook = new Book();
     library.push(newBook);
     console.log(library);
+    dialog.close();
+    resetValues();
+    displayBook();
   }
 
   openDialogBtn.addEventListener('click', () => {
@@ -36,3 +69,5 @@
   });
 
   addBookBtn.addEventListener('click', addBookToLibrary);
+
+  testButton.addEventListener('click', displayBook);
